af2a46e93d57eaf69fc2292619c8fcab84374880
ekaterina-e <127152353+ekaterina-e@users.noreply.github.com>
2023-03-18 19:19:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+// This file is a basic Express server.
 import express from "express";
 import cors from "cors";
 import dictionaryRouter from "./routes/urbanDictionary.js";
 
+// This line of code sets the port.
 const PORT = process.env.PORT || 5000;
 
-// Enable cors
+// In this part, we initialize express and enable cors.
 const app = express();
 app.use(cors());
 
 // Routes
 app.use("/api", dictionaryRouter);
 
+// Here, app listens to PORT, and we have a callback. 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
