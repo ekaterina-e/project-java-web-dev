13145cf3e68d5a25ef3946989384ef982fad52d0
ekaterina-e <127152353+ekaterina-e@users.noreply.github.com>
2023-03-18 20:26:07 +0100

    Update urbanDictionary.js

--- routes/urbanDictionary.js
+++ routes/urbanDictionary.js
@@ -1,12 +1,19 @@
-import express from "express";
+// In the project, we use ESM (JS modules, type="module"),
+that's why we import modules via import() and not via require() (like in CommonJS). */
+import express from "express"; // it's a web app framework that is used to design and build web apps.
 export const router = express.Router();
-import needle from "needle";
-import * as dotenv from "dotenv";
+import needle from "needle"; // it's an HTTP client that is used on the server.
+import * as dotenv from "dotenv"; // allows us to have environment variables.
 dotenv.config();
 
 // Environment variables
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
+/* router.get, where .get refers to GET requests, creates a router.
+We make the function async/await because it returns a Promise.
+req is an object where info about the HTTP request that called the event is contained.
+res is an object that is used to send back the HTTP response.
+*/ 
 router.get("/", async (req, res) => {
   const options = {
     method: "GET",
@@ -21,10 +28,11 @@ router.get("/", async (req, res) => {
   );
   console.log(response);
 
+  // If the response is successful, response.json() returns a JSON object.
   if (response.ok) {
     const data = await response.json();
     res.send(data);
-  } else {
+  } else { // if the response isn't succesful, the user sees "Ooops" on the page.
     res.send("Ooops");
   }
 });
